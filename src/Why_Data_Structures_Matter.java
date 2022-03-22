767fba4e0bfcefdec0670d0b1f1a4439e02abf20
Stephen Gwon <gwonstephen@gmail.com>
2022-03-21 21:56:23 -0400

    pls work

--- src/Why_Data_Structures_Matter.java
+++ src/Why_Data_Structures_Matter.java
@@ -15,7 +15,10 @@ public class Why_Data_Structures_Matter {
     ARRAYS:
         size vs index. size is amount of elements, index is place of element starting at 0
         Data Structure Operations:
-
+            Read- looking for index where value exists
+            Search- looking for value in data structure
+            Insert- add new value
+            Delete- remove value
 
 
     */
