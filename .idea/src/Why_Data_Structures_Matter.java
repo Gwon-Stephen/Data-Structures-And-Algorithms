d77119acb0ffbfa0bec50036e46e077ba72c2b6f
Stephen Gwon <gwonstephen@gmail.com>
2022-03-21 21:43:28 -0400

    pls work

---
+++ .idea/src/Why_Data_Structures_Matter.java
@@ -0,0 +1,22 @@
+public class Why_Data_Structures_Matter {
+    /*
+    Why does this all matter?
+        Code Quality
+            -Maintainability
+                -readability
+                -organization
+                -modularity
+            -Efficiency
+                -speed
+
+        Data is all types of info
+        Data structures refer to how data is organized
+
+    ARRAYS:
+        size vs index. size is amount of elements, index is place of element starting at 0
+        Data Structure Operations:
+
+
+
+    */
+}
