eec76dc4fd86e4afc1c853be25d5a75024b57594
Stephen Gwon <gwonstephen@gmail.com>
2022-03-23 19:51:26 -0400

    asdf
    asdf

---
+++ src/Chapter06_Optimizing_For_Optimistic_Scenarios.java
@@ -0,0 +1,8 @@
+public class Chapter06_Optimizing_For_Optimistic_Scenarios {
+    /*
+    Insertion Sort:
+        -remove the value at index 1,2,3, etc. and set that to a temp variable
+        -take each value to the left, if the value is greater than the temp variable, shift value to the right
+        -ends when we find value that is lower than temp variable or as we reach the left end of the array
+     */
+}
