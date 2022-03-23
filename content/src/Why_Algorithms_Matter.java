125a8ed1df44902a81133ec0620b2d4923df4c30
Stephen Gwon <gwonstephen@gmail.com>
2022-03-22 20:52:42 -0400

    commit
    change to java class

---
+++ content/src/Why_Algorithms_Matter.java
@@ -0,0 +1,34 @@
+public class Why_Algorithms_Matter {
+     /*
+    Algorithm: a set of instructions for completing a specific task.
+
+    Ordered Arrays:
+        -values kept in order
+        -insertion takes N+2 steps, N+1 at the end
+        -linear search
+     */
+
+
+    //implementing linear search on ordered array
+    public static int linear_search(int[] array, int value)
+    {
+        for(int i = 0; i < array.length; i++)
+        {
+            if(i == value)
+                return i;
+            else if(i > value)
+                break;
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args)
+    {
+        int[] orderedArray = {0, 15, 24, 49, 92, 100};
+
+        System.out.println(linear_search(orderedArray, 92));
+        System.out.println(linear_search(orderedArray, 15));
+        System.out.println(linear_search(orderedArray, 80));
+    }
+}
