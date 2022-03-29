870c7ffdd0f36953f7859d88d57d3bb46544a953
Stephen Gwon <gwonstephen@gmail.com>
2022-03-29 00:15:53 -0400

    asdf
    asdf

--- src/Chapter11_Learning_To_Write_In_Recursive.java
+++ src/Chapter11_Learning_To_Write_In_Recursive.java
@@ -10,6 +10,14 @@ public class Chapter11_Learning_To_Write_In_Recursive {
             recursion allows you to find calculation based on a sub-problem of the problem at hand
             subproblem- version of the very same problem applied to a smaller input
             Top Down Recursion-
-                
+                imagine the function you're writing has already been implemented by someone else
+                identify the subproblem of the problem
+                see what happens when function is calls on the subproblem
+            O(N!) => factorial time, slowest category of big O
+
+        Staircase Problem:
+            Staircase with N steps, and a person has teh ability to climb one, two, or three steps. how many different possible paths can someone take to the top?
+            Write a function that will calculate this in N steps
+
      */
 }
