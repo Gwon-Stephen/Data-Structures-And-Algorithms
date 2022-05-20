1f3acd8f5bdcf53d35e31e17c113b3ad4a28eb63
Stephen Gwon <gwonstephen@gmail.com>
2022-05-20 18:35:28 -0400

    asdf

--- src/Chapter13_Recursive_Algorithms_For_Speed.java
+++ src/Chapter13_Recursive_Algorithms_For_Speed.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Chapter13_Recursive_Algorithms_For_Speed {
     /*
     Quicksort- currently fastest way of sorting array, uses partitioning
@@ -14,6 +16,46 @@ public class Chapter13_Recursive_Algorithms_For_Speed {
      */
 
     //Implementing quicksort
-    public static int[] quickSort()
-        
+    public static int partition(int arr[], int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin-1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i+1];
+        arr[i+1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i+1;
+    }
+
+    public static void quickSort(int arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex-1);
+            quickSort(arr, partitionIndex+1, end);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int[] n = {1,3,6,89,4,67,3,8,45,0};
+
+        quickSort(n,0,n.length-1);
+
+        System.out.println(Arrays.toString(n));
+
+    }
+
 }
+
+
