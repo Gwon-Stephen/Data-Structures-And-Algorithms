8f90a103c3dc2cf18257ee7b0ccc5a8422057184
Stephen Gwon <gwonstephen@gmail.com>
2022-05-22 20:05:08 -0400

    asdf

--- src/Chapter19_Space_Constraints.java
+++ src/Chapter19_Space_Constraints.java
@@ -1,2 +1,14 @@
 public class Chapter19_Space_Constraints {
+    /*
+    Space Complexity:
+        how much memory does an algorithm use? aka space complexity
+        Big O notation is used to describe space and time complexity
+            if there are N data elements, how many units of memory will the algorithm consume?
+        O(1)- memory consumed is constant
+        Note: we only count new data algorithm is generating
+            known as auxiliary space
+        there is usually a trade off between time and space and which to sacrifice over the other depends on situation
+        Note: recursion takes up a unit of space for each recursive call it makes, aka takes up a shit ton of memory
+            RangeError: Maximum call stack size exceeded
+     */
 }
---
+++ src/Chapter20_Code_Optimization.java
@@ -0,0 +1,22 @@
+public class Chapter20_Code_Optimization {
+    /*
+    Code Optimization:
+        Determine your big O
+            determine efficiency of current code, known as pre-req
+        Come up with best-conceivable runtime
+        if the best imaginable Big O is faster than your current Big O, try optimizing your code
+        *it isn't always possible to achieve best imaginable runtime
+
+        Using multiple data structures
+            we can use more data structures to store code, making it faster to look up data
+        Recognize Patterns
+            discovering patterns can help cut through complexity
+            generate cases and do trials to find any repetition
+        Greedy Algorithms
+            greedy algorithms is one that in each step, chooses what appears to be the best option at that moment in time
+            just as a better option is found, it drops all the previous options
+        Find an alternative data structure
+            sometimes a different data structure does what you are trying to do better
+            each have their own unique benefits and harms
+     */
+}
